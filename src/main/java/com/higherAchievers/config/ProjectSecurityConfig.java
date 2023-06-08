014d796d25759ffbced270c1a3b5a8487839e854
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-08 22:01:27 +0100

    configured JdbcUserDetailsManager to perform authentication

--- src/main/java/com/higherAchievers/config/ProjectSecurityConfig.java
+++ src/main/java/com/higherAchievers/config/ProjectSecurityConfig.java
@@ -6,10 +6,14 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -24,22 +28,27 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("dummy")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password("dummy")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("dummy")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User
-                .withUsername("user")
-                .password("dummy")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
