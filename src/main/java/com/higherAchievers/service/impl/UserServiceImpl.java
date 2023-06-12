f58368479e49e983270f70df31f2c45aace94619
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-12 16:02:13 +0100

    implemented BCryptPasswordEncoder for registration of new user

--- src/main/java/com/higherAchievers/service/impl/UserServiceImpl.java
+++ src/main/java/com/higherAchievers/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +22,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -43,6 +47,8 @@ public class UserServiceImpl implements UserService {
         Customer savedCustomer = null;
         ResponseEntity<String> response = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
