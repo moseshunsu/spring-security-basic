77c47b0ec5214281039da75f6e6e8f4d238896fb
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-12 15:43:59 +0100

    did some basic refactoring for transitioning into the next section (section_4)

--- src/main/java/com/higherAchievers/config/ProjectSecurityConfig.java
+++ src/main/java/com/higherAchievers/config/ProjectSecurityConfig.java
@@ -5,16 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -29,29 +22,6 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//
-//        UserDetails admin = User
-//                .withUsername("admin")
-//                .password("dummy")
-//                .authorities("admin")
-//                .build();
-//
-//        UserDetails user = User
-//                .withUsername("user")
-//                .password("dummy")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/higherAchievers/service/UserService.java
+++ src/main/java/com/higherAchievers/service/UserService.java
@@ -1,20 +1,10 @@
 package com.higherAchievers.service;
 
 import com.higherAchievers.entity.Customer;
-import com.higherAchievers.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public interface UserService extends UserDetailsService {
     @Override
