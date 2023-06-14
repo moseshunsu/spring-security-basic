4776e16c262376dd41b788c9c529ea0304534a01
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-14 21:18:02 +0100

    removed @EnableWebSecurity annotation in HigherAchieversBankBackEndApplication class

--- src/main/java/com/higherAchievers/HigherAchieversBankBackEndApplication.java
+++ src/main/java/com/higherAchievers/HigherAchieversBankBackEndApplication.java
@@ -2,10 +2,8 @@ package com.higherAchievers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
 public class HigherAchieversBankBackEndApplication {
 
 	public static void main(String[] args) {
