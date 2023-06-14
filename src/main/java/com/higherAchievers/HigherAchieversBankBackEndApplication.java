898b6451537b14a2cf1107c7cb3dfcc12e4f30d5
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-14 16:09:08 +0100

    refactored necessary files for transitioning to the next section (section_8)

--- src/main/java/com/higherAchievers/HigherAchieversBankBackEndApplication.java
+++ src/main/java/com/higherAchievers/HigherAchieversBankBackEndApplication.java
@@ -2,8 +2,10 @@ package com.higherAchievers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class HigherAchieversBankBackEndApplication {
 
 	public static void main(String[] args) {
